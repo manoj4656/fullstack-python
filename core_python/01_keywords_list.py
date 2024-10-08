d434d035ba1753b747db963726f28a3e96790e18
manoj <manoj@example.com>
2024-10-09 00:04:43 +0530

    added string.py file to folder

--- core_python/01_keywords_list.py
+++ core_python/01_keywords_list.py
@@ -1,3 +1,10 @@
 import keyword
 
-print(keyword.kwlist)
+# print keywords
+# print(keyword.kwlist)
+
+# print string functions
+# print(dir(str))
+
+# know about string functions
+help(str)
---
+++ core_python/03_string.py
@@ -0,0 +1,9 @@
+
+s = 'javascript is very easy'
+print(s)
+
+# strings is once we create a string that cannot modify directly
+s1 = s.replace('is', 'is not')
+
+print(s1)
+print(s.replace('is', 'is not'))
