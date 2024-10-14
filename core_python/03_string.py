6a545f05dc13c2fb8c7d47b05883a784a67ee99f
manoj <manoj@example.com>
2024-10-14 16:28:22 +0530

    create table for given user input number

--- core_python/03_string.py
+++ core_python/03_string.py
@@ -7,3 +7,14 @@ s1 = s.replace('is', 'is not')
 
 print(s1)
 print(s.replace('is', 'is not'))
+
+
+# string functions
+# string slicing
+s = 'microsoft'
+print(s[1:5])
+print(s[-8:-4])
+print(s[:])
+
+# reverse string using slicing
+print(s[::-1])
---
+++ core_python/04_create_table.py
@@ -0,0 +1,5 @@
+number = int(input('enter the number to create table: '))
+
+for i in range(1,11):
+    result = number * i
+    print(number,'*',i,'=',result)
